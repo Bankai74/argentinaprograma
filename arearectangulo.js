5db922b01c2805afe48958f104bf19942ac7f307
Bankai74 <jrupprich@gmail.com>
2023-05-23 22:33:12 -0300

    agregue los archivos area ectangulo, miltiplicacion y resta

---
+++ arearectangulo.js
@@ -0,0 +1,30 @@
+// Definir la base y la altura del rectángulo
+let baseA = 5;
+let alturaA = 3;
+
+// Calcular el área del rectángulo
+let areaA = baseA * alturaA;
+
+// Imprimir la operacion en pantalla
+console.log("El rectangulo A tiene una base de " + baseA + "cm y una altura de " + alturaA + "cm lo cual forma una area total de ", areaA + "cm.");
+
+// Definir la base y la altura del rectángulo
+let baseB = 12.5;
+let alturaB = 5.3;
+
+// Calcular el área del rectángulo
+let areaB = baseB * alturaB;
+
+// Imprimir la operacion en pantalla
+console.log("El rectangulo B tiene una base de " + baseB + "cm y una altura de " + alturaB + " cm lo cual forma una area total de", areaB + "cm.");
+
+
+
+
+
+
+
+
+
+
+//node arearectangulo.js
